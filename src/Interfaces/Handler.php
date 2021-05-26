907601bb5b846c5dfce5bd103fd259763e2f1f38
Guglielmo Pepe <1424836+GuglielmoPepe@users.noreply.github.com>
2021-05-26 15:52:15 +0200

    Add handler interface

---
+++ src/Interfaces/Handler.php
@@ -0,0 +1,12 @@
+<?php 
+
+declare(strict_types = 1);
+
+namespace Tiler\Interfaces;
+
+interface Handler
+{
+    public function connect(Handler $handler) : void;
+    public function handle(string $path, \ArrayAccess $data) : string;
+}
+
